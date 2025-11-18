e0b27f08307f918a61d2a52fed402d0e064c69d0
srikanth <srikanth.pamidi534@gmail.com>
2025-11-18 22:12:56 +0530

    Initial commit

---
+++ src/OOP_Examples/objectExp.java
@@ -0,0 +1,36 @@
+package OOP_Examples;
+
+
+
+
+public class objectExp {
+	
+	public int a;
+	public int b;  // global variables.
+	
+	
+	
+	
+	int sum() {
+		return a+b;
+		
+	}
+	
+	int sub() {
+		return a-b;
+		
+	}
+	
+	int mul() {
+		return a*b;
+		}
+	
+	int div() {
+		
+		return a / b;
+		
+	}
+	
+
+}
+  
