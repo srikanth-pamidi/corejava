c3267fd553b80c8bfb2ffc2b138ebb9241f287cd
srikanth <srikanth.pamidi534@gmail.com>
2025-11-19 11:45:43 +0530

    commit

---
+++ src/net/konic/veriables/CarDetail01.java
@@ -0,0 +1,22 @@
+package net.konic.veriables;
+
+public class CarDetail01 {
+	
+	public static void main(String[] args) {
+		
+		
+
+		String brandName = "TATA";
+		int seatingCapacity = 5;
+		int mileage = 15;
+		float cost = 99000.00f;
+
+		System.out.println("Company Name : " + brandName);
+		System.out.println("Seating Capacity : " + seatingCapacity);
+		System.out.println("Mileage Per Liter : " + mileage);
+		System.out.println("Price : " + cost);
+
+	}
+
+
+}
