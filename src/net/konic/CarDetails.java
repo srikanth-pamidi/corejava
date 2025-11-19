92382b0e7fd3a83cf855fffb27dc11679f20e141
srikanth <srikanth.pamidi534@gmail.com>
2025-11-19 11:38:39 +0530

    Create CarDetails.java

---
+++ src/net/konic/CarDetails.java
@@ -0,0 +1,24 @@
+package net.konic;
+
+public class CarDetails {
+	
+	public static void main(String[] args) {
+		
+	
+
+			String brandName = "TATA";
+			int seatingCapacity = 5;
+			int mileage = 15;
+			float cost = 99000.00f;
+
+			System.out.println("Company Name : " + brandName);
+			System.out.println("Seating Capacity : " + seatingCapacity);
+			System.out.println("Mileage Per Liter : " + mileage);
+			System.out.println("Price : " + cost);
+
+		}
+
+
+		
+		
+	}
